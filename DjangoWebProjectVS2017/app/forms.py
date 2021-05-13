af083da6f546a9e7ce22e05751e73e4518460e20
ima98 <44376318+ima98@users.noreply.github.com>
2021-05-13 12:44:02 +0200

    Initial commit

---
+++ DjangoWebProjectVS2017/app/forms.py
@@ -0,0 +1,37 @@
+"""
+Definition of forms.
+"""
+
+from django import forms
+from app.models import Question2,Choice2,User2
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
+
+class QuestionForm(forms.ModelForm):
+
+        class Meta:
+            model = Question2
+            fields = ('question_text','subject','numOptions',)
+
+class ChoiceForm(forms.ModelForm):
+
+        class Meta:
+            model = Choice2
+            fields = ('choice_text','isCorrect',)
+
+class UserForm(forms.ModelForm):
+
+        class Meta:
+            model = User2
+            fields = ('email','nombre',)
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'User name'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Password'}))
---
+++ DjangoWebProjectVS2017/app/models.py
@@ -0,0 +1,37 @@
+"""
+Definition of models.
+"""
+
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+class User(models.Model):
+    email = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+
+
+#copias de los modelos
+class Question2(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    subject= models.CharField(max_length=200, default="")
+
+class Choice2(models.Model):
+    question = models.ForeignKey(Question2)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    isCorrect = models.BooleanField(default=False)
+
+class User2(models.Model):
+    email = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
